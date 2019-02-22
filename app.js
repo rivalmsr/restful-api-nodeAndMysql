7be314dec005c340c7ab4177eca369387cde46e8
rivalmsr <rivalmusripal@gmail.com>
2019-02-22 10:42:21 +0700

    no function

--- app.js
+++ app.js
@@ -1,42 +1,43 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const bodyParser  = require('body-parser');
+const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const path  = require('path');
-const app   = express();
+const path = require('path');
+const app = express();
 
-const {getHomePage} = require('./routes/index');
-const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
+ const {getHomePage} = require('./routes/index');
+ const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
 const port = 5000;
+
 // create connection to database
-// the mysql.createConnection function takes in a configuration object witch contains host, user, password and the database name.
-const db = mysql.createConnection({
-  host : 'localhost',
-  port : 5000,
-  username : 'root',
-  password : '',
-  database : 'dbjavascript'
+// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'socka'
 });
 
 // connect to database
 db.connect((err) => {
-  if(err){
-    throw err;
-  }
-    console.log('connected to database');
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
 });
 global.db = db;
 
-// configuration middleware
+// configure middleware
 app.set('port', process.env.port || port); // set express to use this port
-app.set('views', __dirname + '/views'); // set express to look in this folder to render our views
-app.set('view-engine', 'ejs'); // configure template engine
-app.use(bodyParser.urlencoded({extended : false}));
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'ejs'); // configure template engine
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // parse form data client
-app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public
-app.use(fileUpload()); // configure fileUpload
+app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
+app.use(fileUpload()); // configure fileupload
 
 // routes for the app
+
 app.get('/', getHomePage);
 app.get('/add', addPlayerPage);
 app.get('/edit/:id', editPlayerPage);
@@ -44,7 +45,8 @@ app.get('/delete/:id', deletePlayer);
 app.post('/add', addPlayer);
 app.post('/edit/:id', editPlayer);
 
+
 // set the app to listen on the port
-app.listen(port, ()=> {
-  console.log('Server running on port :' + port);
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
 });
